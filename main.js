0c159d26e0ea7d915b2bca24dbaefbb47a281e48
Deone <deonemontgomery@gmail.com>
2020-11-20 16:49:42 -0600

    updating

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 console.log("Deone");
 
+// Requirements
+// Add a click event listener to the replaceAllButton.
+// On click but outside of a loop, assign the values of the two input textboxes ( findInput and replaceInput), so that you are only retrieving these values once per click.
+// Write a loop which loops over the rowElements array (which is already provided in the starter code).
+// Inside this loop, use the getCellElements() function (already provided in the starter code) and assign the resulting array of cell elements to a variable.
+// Write a nested loop which loops over the array of cell elements.
+// For each cell element, check if a cell contains the user-provided search string. Use the string method includes().
+// If a cell does contain the user-provided search string, use innerHTML and the string method replace() to replace the user-provided search string with the user-provided replacement string.
+// In all your loops, use distinct counters like "i" and "j".
+
 
 let findInput = document.querySelector(".find-input")
 let replaceInput = document.querySelector(".replace-input")
 let replaceAllButton = document.querySelector(".replace-all-button")
-let findValue = findInput.nodeValue;
+let findValue = findInput.Value;
 
 
 
@@ -13,15 +23,17 @@ let findValue = findInput.nodeValue;
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
+
 let rowElements = document.querySelectorAll(".row")
 
 
     
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
     
@@ -30,34 +42,29 @@ function getCellElements (currentRowElement) {
 
 
 // YOUR CODE GOES HERE
+
 replaceAllButton.addEventListener("click", function() {
     let findValue = findInput.value;
-    let replaceValue = replaceInput.nodeValue;
+    let replaceValue = replaceInput.value;
+
+    
 
     for(let i = 0; i < rowElements.length; i++) {
+        // console.log(rowElements[i])
 
-        let cells = getCellElements(rowElements[i])
+        let cellElements = getCellElements(rowElements[i]);
 
         // Write a nested loop which loops over the array of cell elements.
 
-        for(let j = 0; j < cells.length; j++) {
-
-       
+        for(let j = 0; j < cellElements.length; j++) {
 
 
-console.log(cells[j].innerHTML);
-
-            if(cells[j].innerHTML.includes(findInput.value)){
-                // console.log("j");
+        
+            if(cellElements[j].innerHTML.includes(findValue)) {
+                cellElements[j].innerHTML = cellElements[j].innerHTML.replace(findValue, replaceValue)
             }
-            if(cells[j].innerHTML.replace(replaceInput.value)) {
-                // console.log("j");
-            }
-
-         
+            
  
-
-
 
         }
         
